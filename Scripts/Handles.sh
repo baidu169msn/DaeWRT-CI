b258bb0eb983af2ed9f5d0ae5e6fe610e54ae4c1
旅行者久号 <165442377+baidu169msn@users.noreply.github.com>
2026-08-19 16:21:56 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-
 PKG_PATH="$GITHUB_WORKSPACE/$WRT_DIR/package/"
-
-#修改argon主题字体和颜色
-if [ -d "$PKG_PATH/luci-theme-argon" ]; then
-	echo " "
-	if sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" \
-		"$PKG_PATH/luci-theme-argon/luci-app-argon-config/root/etc/config/argon"; then
-		echo "theme-argon has been fixed!"
-	fi
-fi
 
 #修复Rust编译失败 (部分包可能依赖)
 RUST_FILE="$(find "$PKG_PATH/../feeds/packages" -maxdepth 4 -type f -wholename '*/rust/Makefile' -print -quit 2>/dev/null)"
